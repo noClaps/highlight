8d7dd406931cf1e9f7521776a1f7342bd7e0ef2b
noClaps <04plugs-bios@icloud.com>
2024-12-28 23:05:21 +0000

    fix: Add return type for highlight function

--- index.ts
+++ index.ts
@@ -102,7 +102,7 @@ export function highlight(
   code: string,
   language: BundledLanguage,
   theme?: Theme,
-) {
+): string {
   if (!bundledLanguages.includes(language)) {
     throw new Error(`Language ${language} is not supported by Highlight`);
   }
