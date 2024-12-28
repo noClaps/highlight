11fd454c7a36580fb085ac4824068885f22ce112
noClaps <04plugs-bios@icloud.com>
2024-12-28 22:59:19 +0000

    fix: Export symbols from index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { highlight as tsHighlight } from "./dist/index.js";
 
 /** A list of the languages supported by Highlight. */
-const bundledLanguages = [
+export const bundledLanguages = [
   "c",
   "codeql",
   "ql",
@@ -56,10 +56,10 @@ const bundledLanguages = [
 ] as const;
 
 /** The type of bundled language used by the Highlight package */
-type BundledLanguage = (typeof bundledLanguages)[number];
+export type BundledLanguage = (typeof bundledLanguages)[number];
 
 /** The type of theme accepted by the Highlight package. */
-interface Theme {
+export interface Theme {
   fg?: string;
   bg?: string;
   highlights?: Record<
@@ -72,136 +72,6 @@ interface Theme {
     }
   >;
 }
-
-const theme: Theme = {
-  fg: "#fff",
-  bg: "#111",
-  highlights: {
-    attribute: {
-      color: "#ac8e68",
-      fontWeight: 500,
-    },
-    boolean: {
-      color: "#ff375f",
-      fontWeight: 700,
-    },
-    comment: {
-      color: "#98989d",
-      fontWeight: 500,
-    },
-    "comment.doc": {
-      color: "#98989d",
-    },
-    constant: {
-      color: "#6ac4dc",
-      fontWeight: 500,
-    },
-    constructor: {
-      color: "#66d4cf",
-      fontWeight: 500,
-    },
-    enum: {
-      color: "#66d4cf",
-      fontWeight: 500,
-    },
-    emphasis: {
-      color: "#5ac8f5",
-      fontStyle: "italic",
-    },
-    "emphasis.strong": {
-      color: "#5ac8f5",
-      fontWeight: 700,
-    },
-    function: {
-      color: "#66d4cf",
-      fontWeight: 500,
-    },
-    keyword: {
-      color: "#ff375f",
-      fontWeight: 700,
-    },
-    label: {
-      color: "#98989d",
-      fontWeight: 700,
-    },
-    "link.text": {
-      color: "#0a84ff",
-      fontWeight: 500,
-    },
-    "link.uri": {
-      color: "#0a84ff",
-      fontWeight: 500,
-    },
-    link_text: {
-      color: "#ff9f0a",
-      fontWeight: 500,
-    },
-    link_uri: {
-      color: "#0a84ff",
-      fontWeight: 500,
-    },
-    number: {
-      color: "#ffd60a",
-      fontWeight: 500,
-    },
-    operator: {
-      color: "#98989d",
-      fontWeight: 500,
-    },
-    preproc: {
-      color: "#ff9f0a",
-      fontWeight: 500,
-    },
-    property: {
-      color: "#6ac4dc",
-      fontWeight: 500,
-    },
-    "punctuation.bracket": {
-      color: "#98989d",
-      fontWeight: 500,
-    },
-    "punctuation.list_marker": {
-      color: "#ac8e68",
-      fontWeight: 500,
-    },
-    string: {
-      color: "#ff9f0a",
-      fontWeight: 500,
-    },
-    "string.special": {
-      color: "#ff9f0a",
-      fontWeight: 500,
-    },
-    "string.special.symbol": {
-      color: "#ffd60a",
-      fontWeight: 500,
-    },
-    tag: {
-      color: "#ff375f",
-      fontWeight: 700,
-    },
-    "text.literal": {
-      color: "#ff9f0a",
-      fontWeight: 500,
-    },
-    title: {
-      color: "#98989d",
-      fontWeight: 700,
-    },
-    type: {
-      color: "#5ac8f5",
-      fontWeight: 500,
-    },
-    "type.builtin": {
-      color: "#5ac8f5",
-      fontWeight: 500,
-    },
-    variable: {
-      color: "#fff",
-      fontWeight: 500,
-    },
-  },
-};
 
 /**
 A function that takes in code and highlights it.
@@ -270,5 +140,3 @@ export function highlight(
 
   return `<pre class="ts-highlight" style="${globalStyle}"><code>${highlightedText}</code></pre>`;
 }
-
-console.log(highlight("int a = 5;", "c"));
